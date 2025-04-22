83815ba3a39f073568792f824929853efbf54056
prem <c247@prem.lan>
2025-04-22 10:54:55 +0530

    added new PR for inline comments of PRs

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\ProfileRequest;
-use App\Models\User;
 use App\Services\ProfileService;
 use Exception;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class ProfileController extends Controller
 {
@@ -25,7 +21,6 @@ class ProfileController extends Controller
     public function profile()
     {
         try {
-           
             $user = $this->profileService->profile();
             return response()->json(['data' => $user], 200);
         } catch (Exception $e) {
@@ -46,6 +41,48 @@ class ProfileController extends Controller
             return response()->json(['data' => $user, 'message' => 'User Updated Successfully'], 200);
         } catch (Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function complexFunction() {
+        if (true) {
+            if (true) {
+                for ($i = 0; $i < 10; $i++) {
+                    while (true) {
+                        switch ($i) {
+                            case 1:
+                                echo "Nested nightmare!";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public function duplicate1() {
+        echo "Duplicate me!";
+    }
+    
+    public function duplicate2() {
+        echo "Duplicate me!";
+    }
+
+    public function myTesting(){
+        if (true) {
+            if (true) {
+                if (true) {
+                    echo "This is bad";
+                }
+            }
+        }
+    }
+
+    public function badPractice() {
+        if (1 == 1) {
+            echo 'Do something dangerous!';
         }
     }
     
